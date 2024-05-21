4b8f7c970b17215280cc4f8fdbfc5a7dc43c32cf
ma-mrl <marion.mrl@live.fr>
2024-05-21 18:28:36 +0200

    finished css and updated readme.md

--- src/components/SubredditsList/SubredditList.js
+++ src/components/SubredditsList/SubredditList.js
@@ -1,15 +1,15 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getSubreddits, selectIsLoading, selectSubreddits } from '../../features/subreddit/subredditSlice';
-import { LoadingPage } from '../LoadingPage/LoadingPage';
+import { getSubreddits, selectSubreddits } from '../../features/subreddit/subredditSlice';
+
 import { Subreddits } from '../Subreddits/Subreddit';
 
-import './SubredditList.css';
+import '../Post/Post.css';
 
 export const SubredditList = () => {
     const dispatch = useDispatch();
     const subreddits = useSelector(selectSubreddits);
-    const isLoading = useSelector(selectIsLoading)
+
 
     useEffect(() => {
         dispatch(getSubreddits());
@@ -17,23 +17,19 @@ export const SubredditList = () => {
 
     return (
         <aside>
-            {isLoading ? (
-                <LoadingPage />
-            ) : (
-                <div className="post subreddit-post">
-                    <h2>Subreddits</h2>
-                    <ul className="subreddits-list">
-                        {subreddits.map((subreddit, index) => {
-                            return (
-                                <Subreddits 
-                                    key={index}
-                                    name={subreddit.display_name_prefixed}
-                                />
-                            )
-                        })}
-                    </ul>
-                </div>
-            )}
+            <div className="post subreddit-post">
+                <h2>Subreddits</h2>
+                <ul className="subreddits-list">
+                    {subreddits.map((subreddit, index) => {
+                        return (
+                            <Subreddits 
+                                key={index}
+                                name={subreddit.display_name_prefixed}
+                            />
+                        )
+                    })}
+                </ul>
+            </div>
         </aside>
     )
 }
